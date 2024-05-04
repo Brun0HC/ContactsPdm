9d06bfcc4c335581d7eda9654b1dbf34045977d7
Brun0HC <bruno.cobuci@biotronica.tech>
2024-05-04 12:13:25 -0300

    feat: Added 'ipdate' and 'delete' contact functions

--- app/src/main/java/com/example/contactspdm/ui/ContactActivity.kt
+++ app/src/main/java/com/example/contactspdm/ui/ContactActivity.kt
@@ -1,15 +1,21 @@
 package com.example.contactspdm.ui
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.contactspdm.databinding.ActivityContactBinding
 import com.example.contactspdm.model.Contact
+import com.example.contactspdm.model.Constant.EXTRA_CONTACT
+import com.example.contactspdm.model.Constant.EXTRA_VIEW_CONTACT
+import android.view.View
 
 class ContactActivity: AppCompatActivity() {
     private val acb: ActivityContactBinding by lazy {
         ActivityContactBinding.inflate(layoutInflater)
     }
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(acb.root)
@@ -17,23 +23,47 @@ class ContactActivity: AppCompatActivity() {
         subtitle = this@ContactActivity.javaClass.simpleName
         setSupportActionBar(this)
     }
-
-        with(acb) {
-            acb.saveBt.setOnClickListener {
-                Contact(
-                    id = hashCode(),
-                    name = nameEt.text.toString(),
-                    address = addressEt.text.toString(),
-                    phone = phoneEt.text.toString(),
-                    email = emailEt.text.toString()
-                ).let { contact ->
-                    Intent().apply {
-                        putExtra("EXTRA_CONTACT", contact)
-                        setResult(RESULT_OK, this)
-                        finish()
-                    }
+    val receiveContact = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        intent.getParcelableExtra(EXTRA_CONTACT, Contact::class.java)
+    }
+    else {
+        intent.getParcelableExtra(EXTRA_CONTACT)
+    }
+        with(acb){
+            receiveContact?.let {
+                nameEt.setText(receiveContact.name)
+                addressEt.setText(receiveContact.address)
+                phoneEt.setText(receiveContact.phone)
+                emailEt.setText(receiveContact.email)
+                if (intent.getBooleanExtra(EXTRA_VIEW_CONTACT, false)){
+                    nameEt.isEnabled = false
+                    addressEt.isEnabled = false
+                    phoneEt.isEnabled = false
+                    emailEt.isEnabled = false
+                    saveBt.visibility = View.GONE
                 }
+
             }
+            saveBt.setOnClickListener{Contact(
+                id =  receiveContact?.id?: hashCode(),
+                name = nameEt.text.toString(),
+                address = addressEt.text.toString(),
+                phone = phoneEt.text.toString(),
+                email = emailEt.text.toString()
+            ).let {  contact ->
+                Intent().apply {
+                    putExtra(EXTRA_CONTACT,contact)
+                    setResult(RESULT_OK, this)
+                    finish()
+                }
+
+            }
+
+            }
+
+
         }
+
     }
 }
+
